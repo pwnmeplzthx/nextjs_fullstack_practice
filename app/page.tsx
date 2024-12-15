3599fc692eabf3dbf6a962c7afd95bcd57ded79f
pwnmeplzthx <pshumakov11@mail.ru>
2024-12-15 22:25:45 +0300

    project init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <h1>
+            Home
+            <Button variant={"default"}>???</Button>
+        </h1>
+    
+    );
+}
